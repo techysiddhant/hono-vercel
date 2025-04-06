4afbf3fbafe65a9420ce6e624fff4efa131ee672
Siddhant Jain <sidjain8000@gmail.com>
2025-04-06 17:03:57 +0530

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { handle } from "hono/vercel";
+import { handle } from "@hono/node-server/vercel";
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-expect-error
 import app from "../dist/src/index.js";
